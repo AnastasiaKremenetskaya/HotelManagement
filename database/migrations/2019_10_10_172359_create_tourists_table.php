5a378fc035c3c76c538ffa5142379f4f8638611e
AnastasiaKremenetskaya <46082850+AnastasiaKremenetskaya@users.noreply.github.com>
2019-10-10 21:30:22 +0400

    Added migrations for Room and Tourist. Created model and controller templates.

---
+++ database/migrations/2019_10_10_172359_create_tourists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTouristsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tourists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('patronymic');
+            $table->string('citizenship');
+            $table->date('date_of_birth');
+            $table->boolean('is_vip');
+            $table->integer('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tourists');
+    }
+}
